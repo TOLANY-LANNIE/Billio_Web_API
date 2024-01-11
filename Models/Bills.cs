aa3f8e0db2e98ad468a1e70ae86dc12d194a3a46
Thulani Mpofu <tolanym_2@live.com>
2024-01-11 14:34:11 +0200

    Thulani| addede Trans, Bills and Users models

---
+++ Models/Bills.cs
@@ -0,0 +1,48 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BillioAPI.Models;
+
+
+public class Bills{
+
+
+    /// <summary>
+    /// Get or set the <see cref="string"/> primary Id property
+    /// </summary> 
+    [BsonId] 
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id{get;set;}
+
+    /// <summary>
+    ///  Get or set the <see cref="string"/>  Bill Description property
+    /// </summary>
+    public string description{get;set;} = null!;
+
+    /// <summary>
+    ///  Get or set the <see cref="Date"/>  Bill Due Date property
+    /// </summary>
+    public Date due_date{get;set;}
+
+    /// <summary>
+    ///  Get or set the <see cref="string"/>  Bill Budget property
+    /// </summary>
+    public string budget_id{get; set;}
+
+    /// <summary>
+    ///  Get or set the <see cref="decimal"/>  Bill Amount property
+    /// </summary>
+    public decimal amount{get;set;}
+
+    /// <summary>
+    ///  Get or set the <see cref="string"/>  Bill Status property
+    /// </summary>
+    public string status{get;set;} = null!;
+
+    /// <summary>
+    ///  Get or set the <see cref="string"/>  Bill User property
+    /// </summary>
+    public string user_id{get;set;} = null!;
+
+}
---
+++ Models/Users.cs
@@ -0,0 +1,5 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BillioAPI.Models;
